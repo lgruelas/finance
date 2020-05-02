423179bfb9a225810376af4bdc79ce0e35f42828
Germán Ruelas <lgruelas@hotmail.com>
2020-05-02 11:58:15 -0500

    Remains cors error

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!a+60e%)%1^c=-a4*kix(+$75oi+%8o6f*cxi7521fy!32hiy)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['worker', 'localhost']
 
 
 # Application definition
