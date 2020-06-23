7ec9ac6d64b7cae5383ddbbedf7e96420b1327a8
Germán Ruelas <lgruelas@hotmail.com>
2020-06-22 20:00:42 -0500

    added flake8 fail to test it

--- backend/finance/tests/test_views/test_income.py
+++ backend/finance/tests/test_views/test_income.py
@@ -122,7 +122,7 @@ class IncomeViewTest(TestCase):
     @patch('finance.views.IncomeView.get_serializer')
     @patch('finance.views.IncomeView.get_object')
     def test_update_with_same_account_and_balance(self, mock_instance, mock_serializer, mock_get_object, mock_update):
-        request = self.factory.put("some_url", data=self.income_data)
+        request = self.factory.put("some_url this string is intended to bee too long so flake8 will fail", data=self.income_data)
         # New objects to be changed
         old_income = Mock(
             amount=10,
