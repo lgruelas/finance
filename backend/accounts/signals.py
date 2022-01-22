b47e85efc30cab74cbd92e6ecbd9ccd5faa55b13
Germán Ruelas <lgruelas@hotmail.com>
2022-01-22 01:38:08 -0600

    Handle update

--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -18,7 +18,7 @@ def update_old_account_balance_from_income(instance, old_account_balance, old_ac
     Account.objects.filter(id=old_account_id).update(balance=fixed_balance)
 
 
-def update_new_account_balance_from_income(instance, instance_amount, account_name="account")
+def update_new_account_balance_from_income(instance, instance_amount, account_name="account"):
     getattr(instance, account_name).balance += instance_amount
     getattr(instance, account_name).save()
 
@@ -31,7 +31,7 @@ def update_old_account_balance_from_expense(instance, old_account_balance, old_a
     Account.objects.filter(id=old_account_id).update(balance=fixed_balance)
 
 
-def update_new_account_balance_from_expense(instance, instance_amount, account_name="account")
+def update_new_account_balance_from_expense(instance, instance_amount, account_name="account"):
     getattr(instance, account_name).balance -= instance_amount
     getattr(instance, account_name).save()
 
@@ -48,7 +48,7 @@ def saves_old_income_if_update(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Expense)
 def saves_old_expense_if_update(sender, instance, **kwargs):
-    if is_admin_update(instance, **kwargs):
+    if is_update(instance, **kwargs):
         old_expense = Expense.objects.select_related("account").get(id=instance.id)
         instance.old_amount = old_expense.amount
         if instance.account.id != old_expense.account.id:
@@ -57,7 +57,7 @@ def saves_old_expense_if_update(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Transfer)
 def saves_old_transfer_if_update(sender, instance, **kwargs):
-    if is_admin_update(instance, **kwargs):
+    if is_update(instance, **kwargs):
         old_transfer = Transfer.objects.select_related("account_to", "account_from").get(id=instance.id)
         instance.old_amount = old_transfer.amount
         if instance.account_to.id != old_transfer.account_to.id:
@@ -74,8 +74,8 @@ def increase_balance_on_income(sender, instance, created, **kwargs):
         if instance.old_account_id:
             update_old_account_balance_from_income(instance, instance.old_account_balance, instance.old_account_id)
             update_new_account_balance_from_income(instance, instance.amount)
-        elif intance.old_amount:
-            update_new_account_balance_from_income(instance, insntance.old_amount - instance.amount)
+        elif instance.old_amount:
+            update_new_account_balance_from_income(instance, instance.old_amount - instance.amount)
 
 
 @receiver(post_save, sender=Expense)
@@ -86,8 +86,8 @@ def decrease_balance_on_expense(sender, instance, created, **kwargs):
         if instance.old_account_id:
             update_old_account_balance_from_expense(instance, instance.old_account_balance, instance.old_account_id)
             update_new_account_balance_from_expense(instance, instance.amount)
-        elif intance.old_amount:
-            update_new_account_balance_from_expense(instance, insntance.old_amount -  instance.amount)
+        elif instance.old_amount:
+            update_new_account_balance_from_expense(instance, instance.old_amount - instance.amount)
 
 
 @receiver(post_save, sender=Transfer)
@@ -97,13 +97,17 @@ def update_balance_on_transfer(sender, instance, created, **kwargs):
         update_new_account_balance_from_expense(instance, instance.amount, "account_from")
     elif is_update(instance, **kwargs):
         if instance.old_account_id:
-            update_old_account_balance_from_income(instance, instance.old_account_to_balance, instance.old_account_to_id)
-            update_old_account_balance_from_expense(instance, instance.old_account_from_balance, intance.old_account_from_id)
+            update_old_account_balance_from_income(instance,
+                                                   instance.old_account_to_balance,
+                                                   instance.old_account_to_id)
+            update_old_account_balance_from_expense(instance,
+                                                    instance.old_account_from_balance,
+                                                    instance.old_account_from_id)
             update_new_account_balance_from_income(instance, instance.amount, "account_to")
             update_new_account_balance_from_expense(instance, instance.amount, "account_from")
-        elif intance.old_amount:
-            update_new_account_balance_from_income(instance, insntance.old_amount -  instance.amount, "account_to")
-            update_new_account_balance_from_expense(instance, insntance.old_amount -  instance.amount, "account_from")
+        elif instance.old_amount:
+            update_new_account_balance_from_income(instance, instance.old_amount - instance.amount, "account_to")
+            update_new_account_balance_from_expense(instance, instance.old_amount - instance.amount, "account_from")
 
 
 @receiver(post_delete, sender=Income)
